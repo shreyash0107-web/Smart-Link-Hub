e2507601c6de0274853a44342ddd2eeb9a37a798
Shreyash Kumar <shreyashkumarjee@gmail.com>
2026-01-28 09:50:03 +0530

    Fix frontend source of truth

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Serve frontend files
-app.use(express.static(path.join(__dirname, "../Frontend")));
+app.use(express.static(path.join(__dirname, "..Frontend")));
 
 // Default route → open login page
 app.get("/", (req, res) => {
